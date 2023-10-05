940e05d8a1f4329de57c458ce21674ba37946764
David Wijaya <davidwijaya0801@gmail.com>
2023-10-05 12:55:46 +0700

    change portofolio projects

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -21,19 +21,19 @@ function Portfolio() {
 
 						<div className="portfolio-wrapper">
 							<div className="portfolio-item">
-								<img src="/chitchat.png" className="portfolio-image" />
+								<img src="/gotravl.png" className="portfolio-image" />
 
 								<h4 className="portfolio-name">Go Travl</h4>
 								<div className="portfolio-category">Mobile Dev</div>
 							</div>
 							<div className="portfolio-item">
-								<img src="/kajian.jpg" className="portfolio-image" />
+								<img src="/beveragebliss.jpg" className="portfolio-image" />
 
 								<h4 className="portfolio-name">Beverage Bliss</h4>
 								<div className="portfolio-category">Web Dev</div>
 							</div>
 							<div className="portfolio-item">
-								<img src="/kajian.jpg" className="portfolio-image" />
+								<img src="/subway-mobile.jpg" className="portfolio-image" />
 
 								<h4 className="portfolio-name">Subway Mobile App</h4>
 								<div className="portfolio-category">Mobile Dev</div>
