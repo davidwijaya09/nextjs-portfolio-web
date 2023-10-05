f8132e3718b64c3bcc981d8cae3f0b188ed36db7
David Wijaya <davidwijaya0801@gmail.com>
2023-10-05 14:40:03 +0700

    fix: adding little change on portfolio

--- pages/portfolio.jsx
+++ pages/portfolio.jsx
@@ -38,6 +38,18 @@ function Portfolio() {
 								<h4 className="portfolio-name">Subway Mobile App</h4>
 								<div className="portfolio-category">Mobile Dev</div>
 							</div>
+							<div className="portfolio-item">
+								<img src="/sas.png" className="portfolio-image" />
+
+								<h4 className="portfolio-name">Subway Admin</h4>
+								<div className="portfolio-category">Web Dev</div>
+							</div>
+							<div className="portfolio-item">
+								<img src="/job-search.png" className="portfolio-image" />
+
+								<h4 className="portfolio-name">Job Search</h4>
+								<div className="portfolio-category">Web Dev</div>
+							</div>
 						</div>
 					</div>
 				</div>
